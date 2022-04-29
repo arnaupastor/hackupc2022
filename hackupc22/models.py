4c2a1246ea105f842656936c83fc8acb69a8c346
Arnau Pastor <arnaupastor>
2022-04-30 01:16:12 +0200

    eliminao

--- hackupc22/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.db import connection
-
-# Create your models here.
-raw_query = "SELECT * FROM brands LIMIT 10"
-
-cursor = connection.cursor()
-cursor.execute(raw_query)
-cursor.fetchall()
