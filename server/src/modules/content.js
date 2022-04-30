027328c2c765c01b951da5928375e47441a7105a
Arnau Pastor <arnaupastor>
2022-04-30 13:12:16 +0200

    començament content

--- server/src/modules/content.js
+++ server/src/modules/content.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const users = require(`./users.json`);
+const {sql} = require(`../db.js`)
 
 // --------------------------
 // GET API
@@ -8,12 +9,55 @@ const users = require(`./users.json`);
 
 const USER_ID = 1;
 
+function idValorats(user) {
+    let ids = [];
+    for (let i = 0; i < length(user); ++i) {
+        ids.push(user[i].moto_id);
+    }
+
+    return ids;
+}
+
+function cleanDataset(motos, motosValorades) {
+    let motosClean = [];
+    for (let i = 0; i < length(motos); ++i) {
+        if (!motosValorades.includes(parseInt(motos[i].id))) {
+            motosClean.push(motos[i]);
+        }
+    }
+
+    return motosClean;
+}
+
+function cosSimiliarity() {
+
+}
+
+function nameSimilarity() {
+
+}
+
+function knn(motos, id) {
+
+}
+
 router.get("/content",
     async (req, res) => {
 
     const user = users.find(u => u.user_id = USER_ID);
+    const motos = await sql.any(`SELECT * FROM versions LIMIT 10`);
 
-    res.send(user);
+    motosValorades = idValorats(user);
+    motosClean = cleanDataset(motos, motosValorades);
+    
+    let k = 3;
+    for (let i = 0; i < this.length(user); ++i) {
+        
+    }
+
+    console.log("Content!");
+    res.send(motos);
+    //res.send(user);
 
 });
 
